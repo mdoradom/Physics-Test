9cc2c856ab1df99069ed09da1de429683b0d6bec
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2023-09-25 11:22:54 +0200

    Initial commit

---
+++ Project_9_Handout/Source/SceneLevel1.cpp
@@ -0,0 +1,67 @@
+#include "SceneLevel1.h"
+
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "ModuleAudio.h"
+#include "ModuleCollisions.h"
+#include "ModuleEnemies.h"
+#include "ModulePlayer.h"
+
+SceneLevel1::SceneLevel1(bool startEnabled) : Module(startEnabled)
+{
+
+}
+
+SceneLevel1::~SceneLevel1()
+{
+
+}
+
+// Load assets
+bool SceneLevel1::Start()
+{
+	LOG("Loading background assets");
+
+	bool ret = true;
+
+	bgTexture = App->textures->Load("Assets/Sprites/background.png");
+	App->audio->PlayMusic("Assets/Music/stage1.ogg", 1.0f);
+
+	// Add colliders
+	App->collisions->AddCollider({ 0, 224, 3930, 16 }, Collider::Type::WALL);
+
+	// Remder camear
+	App->render->camera.x = 0;
+	App->render->camera.y = 0;
+
+	App->player->Enable();
+	App->enemies->Enable();
+
+	return ret;
+}
+
+Update_Status SceneLevel1::Update()
+{
+	// if wanted to move the camera
+	//App->render->camera.x += 3;
+
+	return Update_Status::UPDATE_CONTINUE;
+}
+
+// Update: draw background
+Update_Status SceneLevel1::PostUpdate()
+{
+	// Draw everything --------------------------------------
+	App->render->Blit(bgTexture, 0, 0, NULL);
+
+	return Update_Status::UPDATE_CONTINUE;
+}
+
+bool SceneLevel1::CleanUp()
+{
+	App->player->Disable();
+	App->enemies->Disable();
+
+	return true;
+}
